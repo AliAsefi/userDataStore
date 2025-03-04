609f94c3590df2749c853d0c855a80e995d1b46a
AliAsefi <asefi.alireza64@gmail.com>
2025-03-04 12:23:09 +0100

    3.version of project fixed corsMapping Users, Investment, Loans, Payment

--- src/main/java/com/example/userDataStore/configuration/CorsConfig.java
+++ src/main/java/com/example/userDataStore/configuration/CorsConfig.java
@@ -16,7 +16,7 @@ public class CorsConfig {
                         .allowedOrigins("http://127.0.0.1:5500") // Allow your frontend
                         //.allowedOrigins("http://localhost:5500") // If using Live Server
                         //.allowedOrigins("http://localhost:8080") // If frontend is hosted elsewhere
-                        //.allowedOrigins("https://aliasefi.github.io")
+                        .allowedOrigins("https://aliasefi.github.io")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowCredentials(true);
             }
